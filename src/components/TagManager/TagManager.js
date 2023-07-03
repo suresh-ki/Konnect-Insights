ab3f6b2641ec749876006809c8050812960c1cd4
kushagra43 <63740432+kushagra43@users.noreply.github.com>
2023-07-03 14:33:20 +0530

    Update TagManager.js

--- src/components/TagManager/TagManager.js
+++ src/components/TagManager/TagManager.js
@@ -6,14 +6,14 @@ const TagManager = () => {
   return (
     <>
       <Head>
-<script>
+<Script>
 (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-MXPKVXV');</script>
 <!-- End Google Tag Manager -->
-</script>
+</Script>
     
     </Head>
 
