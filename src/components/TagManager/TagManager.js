6fde211567658cef0a69f60e6c2f5d3628e0fa1a
Mr.Nayra <aryanedu5786@gmail.com>
2023-07-03 15:15:00 +0530

    google tag manager

--- src/components/TagManager/TagManager.js
+++ src/components/TagManager/TagManager.js
@@ -1,25 +1,29 @@
-import React from 'react';
-import Head, { NoScript } from 'next/head';
-import Script from 'next/script';
+import React from "react";
+import Head, { NoScript } from "next/head";
+import Script from "next/script";
 
 const TagManager = () => {
   return (
     <>
-<Head>
-<Script>
+      <Script
+        id="googletag"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
   (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
   new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
   j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
   'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-  })(window,document,'script','dataLayer','GTM-MXPKVXV');
-</Script>
-  </Head>    
+  })(window,document,'script','dataLayer','GTM-MXPKVXV');`,
+        }}
+      ></Script>
 
-<!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MXPKVXV"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
-  
+      <noscript
+        dangerouslySetInnerHTML={{
+          __html: `<<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MXPKVXV"
+height="0" width="0" style="display:none;visibility:hidden"></iframe`,
+        }}
+      ></noscript>
     </>
   );
 };
