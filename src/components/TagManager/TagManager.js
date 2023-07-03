c4aca1374ab9a68dd9bfe68b005b5dd380d64cdd
kushagra43 <63740432+kushagra43@users.noreply.github.com>
2023-07-03 14:31:49 +0530

    Create TagManager.js

---
+++ src/components/TagManager/TagManager.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Head from 'next/head';
+import Script from 'next/script';
+
+const TagManager = () => {
+  return (
+    <>
+      <Head>
+<script>
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MXPKVXV');</script>
+<!-- End Google Tag Manager -->
+</script>
+    
+    </Head>
+
+    <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MXPKVXV"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
+    
+    </>
+  );
+};
+
+export default TagManager;
