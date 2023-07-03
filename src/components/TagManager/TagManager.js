de7aff6e43be30d0e94d8bd67f1bb952091ac3dd
kushagra43 <63740432+kushagra43@users.noreply.github.com>
2023-07-03 14:48:01 +0530

    Update TagManager.js

--- src/components/TagManager/TagManager.js
+++ src/components/TagManager/TagManager.js
@@ -11,7 +11,7 @@ const TagManager = () => {
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-MXPKVXV');</script>
+})(window,document,'script','dataLayer','GTM-MXPKVXV');
 <!-- End Google Tag Manager -->
 </Script>
 
