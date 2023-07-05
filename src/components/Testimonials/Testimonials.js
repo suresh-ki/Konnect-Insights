885d60300e908c6639b0d0c403bdeb40269ec086
Mr.Nayra <aryanedu5786@gmail.com>
2023-07-05 20:07:08 +0530

    changes -4

--- src/components/Testimonials/Testimonials.js
+++ src/components/Testimonials/Testimonials.js
@@ -7,14 +7,110 @@ import { Autoplay } from "swiper";
 
 const Card = (props) => {
   return (
-    <div className="border-2 border-[#E9E9E9] rounded-[10px] h-[400px] sm:h-[415px] flex flex-col justify-between">
+    <div className="border-2 border-[#E9E9E9] rounded-[10px] h-[480px] sm:h-[500px] md:h-[520px] lg:h-[514px] flex flex-col justify-between">
       <div className="p-10">
-        <p className="text-[#404040] text-sm md:text-base leading-tight md:leading-tight">
-          {props.text}
-        </p>
+        {props.index == 0 && (
+          <p className="text-[#404040] text-sm md:text-base leading-tight md:leading-tight">
+            <span className="font-semibold">Konnect Insights</span> is
+            incredibly <span className="font-semibold">powerfull!</span> I have
+            been actively using the platform for some time now and what has
+            impressed me{" "}
+            <span className="font-semibold">
+              the most is the in-depth reporting from social listening
+            </span>{" "}
+            across channels. You can really dig in and gain actionable insights
+            to deepen connection with your followers.{" "}
+            <span className="font-semibold">
+              The publishing tools are excellent,
+            </span>{" "}
+            too.
+          </p>
+        )}
+        {props.index == 1 && (
+          <p className="text-[#404040] text-sm md:text-base leading-tight md:leading-tight">
+            Konnect Insights'{" "}
+            <span className="font-semibold">
+              main USP is that it has an amalgamation of multiple modules.
+            </span>{" "}
+            It was a blessing in disguise to have accidentally stepped on its
+            website. The <span className="font-semibold">smooth UX/UI</span>{" "}
+            encourages any onlooker to stay put with the tools.{" "}
+            <span className="font-semibold">
+              Gaining Insights and generating reports has become effortless,
+            </span>{" "}
+            we are happy to have chosen Konnect Insights as our Social Listening
+            Partner.
+          </p>
+        )}
+        {props.index == 2 && (
+          <p className="text-[#404040] text-sm md:text-base leading-tight md:leading-tight">
+            The biggest advantage we see with using Konnect Insights is the
+            hands-on approach of the team. They have{" "}
+            <span className="font-semibold">
+              taken into account our requirements and included the same in their
+              product development roadmap.
+            </span>{" "}
+            The platform is{" "}
+            <span className="font-semibold">
+              easy to use and has added to our overall social intelligence,
+            </span>{" "}
+            and we aim to use the data{" "}
+            <span className="font-semibold">
+              we have to provide impactful insights
+            </span>{" "}
+            to our clients.
+          </p>
+        )}
+        {props.index == 3 && (
+          <p className="text-[#404040] text-sm md:text-base leading-tight md:leading-tight">
+            Konnect Insights has been a very helpful{" "}
+            <span className="font-semibold">
+              360-degree tool for our marketing and customer support teams.
+            </span>{" "}
+            It helped us{" "}
+            <span className="font-semibold">
+              understand the current market sentiments
+            </span>{" "}
+            towards our brand and to create effective strategies around it.{" "}
+            <span className="font-semibold">
+              The best feature of this tool would be their Reports Dashboard
+            </span>{" "}
+            that has a vast variety of data charts that can be used to create
+            reports and presentations whenever needed.
+          </p>
+        )}
+        {props.index == 4 && (
+          <p className="text-[#404040] text-sm md:text-base leading-tight md:leading-tight">
+            Konnect Insights is primarily a{" "}
+            <span className="font-semibold">
+              professional social listening and monitoring tool
+            </span>
+            ; but, like a Swiss army knife, it actually includes a few other
+            full-fledged services as well: Social CRM tools: workflow and
+            response management features; Analytics: brand, competition, and
+            industry analysis; Social scheduling: easily schedule messages for
+            your social profiles.
+          </p>
+        )}
+        {props.index == 5 && (
+          <p className="text-[#404040] text-sm md:text-base leading-tight md:leading-tight">
+            Konnect Insights is proving to be essential to my job - right from
+            getting <span className="font-semibold">exhaustive coverage</span>{" "}
+            for my clients to being able to{" "}
+            <span className="font-semibold">
+              manage their reputation by responding to customer queries.
+            </span>{" "}
+            Most importantly I find it to be intuitive, so if there is anything
+            that I haven't been shown I can generally figure it out for myself.
+            It is{" "}
+            <span className="font-semibold">
+              the best software of its kind that I have ever used.
+            </span>
+          </p>
+        )}
       </div>
       <div className="border-t-2 py-7 px-10 h-[130px] flex gap-2.5">
-        <img src={props.img} className="w-16 h-16 object-cover" />
+        <img src={props.img} className="w-16 h-16 object-cover rounded-full" />
         <div>
           <h3 className="text-[#070707] font-semibold lg:text-lg">
             {props.name}
@@ -33,29 +129,34 @@ const Testimonials = () => {
 
   const data = [
     {
-      text: "We have a seamless integration between Konnect Insights and Freshdesk. This gives us the ability to provide excellent coverage to our clients when it comes to social data. The queries coming on social media and web can be easily pushed into Freshdesk thus giving us a single view of the customer across the systems",
-      name: "Eric Bohren's",
-      position:
-        "Freshworks - Sr Director, Global Technology Partners and Marketplace",
-      img: "/Eric.png",
+      name: "Todd Nilson",
+      position: "Clocktower Advisors- President",
+      img: "/Todd.png",
     },
     {
-      text: "Konnect Insights is proving to be essential to my job - right from getting exhaustive coverage for my clients to being able to manage their reputation by responding to customer queries. Most importantly I find it to be intuitive, so if there is anything that I haven't been shown I can generally figure it out for myself. It is the best software of its kind that I have ever used.",
-      name: "Keith",
-      position: "Tangram Consulting- Marketing & Social Media Manager",
-      img: "/Keith.png",
+      name: "Jithin",
+      position: "Decathlon- Country Feedback Leader",
+      img: "/Jithin.png",
     },
     {
-      text: "“Konnect Insights is primarily a professional social listening and monitoring tool; but, like a Swiss army knife, it actually includes a few other full-fledged services as well:Social CRM tool: workflow and response management features; Analytics: brand, competition, and industry analysis; Social scheduling: easily schedule messages for your social profiles.”",
+      name: "GD Prasad",
+      position: "Dentsu Webchutney- Senior Account Director",
+      img: "/GD-Prasad.png",
+    },
+    {
+      name: "Maria Carvalho",
+      position: "Cuemath- Associate Manager Marketing",
+      img: "/Maria.jpeg",
+    },
+    {
       name: "Lilach Bullock",
       position: "Lilach Bullock Ltd- Founder",
       img: "/Lilach.png",
     },
     {
-      text: "“Konnect Insights is incredibly powerful! I have been actively using the platform for some time now and what has impressed me the most is the in-depth reporting from social listening across channels. You can really dig in and gain actionable insights to deepen connection with your followers. The publishing tools are excellent, too.”",
-      name: "Todd Nilson",
-      position: "Clocktower Advisors- President",
-      img: "/Todd.png",
+      name: "Keith",
+      position: "Tangram Consulting- Marketing & Social Media Manager",
+      img: "/Keith.png",
     },
   ];
 
@@ -95,6 +196,7 @@ const Testimonials = () => {
                 name={item.name}
                 position={item.position}
                 img={item.img}
+                index={index}
               />
             </SwiperSlide>
           ))}
