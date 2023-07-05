93007e68e016d555dbf0258c212bbc0aed702ac7
Mr.Nayra <aryanedu5786@gmail.com>
2023-07-05 22:08:24 +0530

    added tag manager

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,22 @@
+import { Suspense } from "react";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import TagManager from "@/components/TagManager/TagManager";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Konnect Insights - An omni-channel customer experience management platform",
+  title:
+    "Konnect Insights - An omni-channel customer experience management platform",
   description: "An omni-channel customer experience management platform",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Suspense>
+        <TagManager />
+      </Suspense>
       <body className={inter.className}>{children}</body>
     </html>
   );
--- src/components/TagManager/TagManager.js
+++ src/components/TagManager/TagManager.js
@@ -1,31 +1,42 @@
-import React from "react";
-import Head, { NoScript } from "next/head";
+"use client";
 
-const TagManager = () => {
+import { usePathname, useSearchParams } from "next/navigation";
+import Script from "next/script";
+import { useEffect } from "react";
+
+export default function Analytics() {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    if (pathname) {
+      pageview(pathname);
+    }
+  }, [pathname, searchParams]);
+
   return (
     <>
-      <Head>
-        <script
-          id="googletag"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-  })(window,document,'script','dataLayer','GTM-MXPKVXV');`,
-          }}
-        ></script>
-      </Head>
-      <noscript
+      <noscript>
+        <iframe
+          src={`https://www.googletagmanager.com/ns.html?id=GTM-MXPKVXV`}
+          height="0"
+          width="0"
+          style={{ display: "none", visibility: "hidden" }}
+        />
+      </noscript>
+      <Script
+        id="gtm-script"
+        strategy="afterInteractive"
         dangerouslySetInnerHTML={{
-          __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MXPKVXV"
-height="0" width="0" style="display:none;visibility:hidden"></iframe`,
+          __html: `
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer', 'GTM-MXPKVXV');
+  `,
         }}
-      ></noscript>
+      />
     </>
   );
-};
-
-export default TagManager;
+}
