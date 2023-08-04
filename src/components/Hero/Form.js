572efe010e907469af5fbfcd5c921039500938f7
kushagra43 <63740432+kushagra43@users.noreply.github.com>
2023-08-04 19:25:10 +0530

    Update Form.js

--- src/components/Hero/Form.js
+++ src/components/Hero/Form.js
@@ -93,7 +93,6 @@ const Form = () => {
               (window.location.href = "https://app.konnectinsights.com/Signup")
             }
             className="w-56"
-                          id = "product_button"
           >
             Explore the product
           </Button>
@@ -102,7 +101,6 @@ const Form = () => {
               (window.location.href = "https://konnectinsights.com/")
             }
             className="w-56"
-            id = "demo_button"
           >
             Book a demo
           </Button>
